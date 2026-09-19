748e7bf2952ee5ca25463d5152d6014aae875d4b
krishantha <krishantha@live.com>
2026-09-19 14:47:01 +0530

    fixing deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig(({ mode }) => {
       setupFiles: './src/test/setup.ts',
     },
     build: {
+      outDir: 'build',
       rollupOptions: {
         output: {
           manualChunks: {
